b5e7d1b80a0631138df660a0f2d112e687fba6ba
Tafsirchy <tafsirchy1000@gmail.com>
2025-11-01 01:10:21 +0600

    navbar and footer added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,9 +1,9 @@
 import { createBrowserRouter } from "react-router";
-import App from "../App";
 import Home from "../Pages/Home";
 import Products from "../Pages/Products";
 import MainLayouts from "../Layouts/MainLayouts";
 import ErrorPage from "../Pages/ErrorPage";
+import Wishlist from "../Pages/Wishlist";
 
 // named export
 const router = createBrowserRouter([
@@ -15,17 +15,22 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <Home></Home>,
+        loader: () => fetch("./furnitureData.json")
       },
       {
         path: "/products",
         element: <Products></Products>,
       },
+      {
+        path: "/wishlist",
+        element: <Wishlist></Wishlist>,
+      },
     ],
   },
-  {
-    path: '*',
-    element: <ErrorPage></ErrorPage>
-  }
+  // {
+  //   path: '*',
+  //   element: <ErrorPage></ErrorPage>
+  // }
 ]);
 
 export default router;
